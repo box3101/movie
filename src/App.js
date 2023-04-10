0b5f0a8ed136789db4db9152efde96dbd8fa96a5
cylee_ispark <tony.at.cp@gmail.com>
2023-04-10 16:10:07 +0900

    todo 리스트 완성.

--- src/App.js
+++ src/App.js
@@ -1,47 +1,44 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
-// 리액트는 새로운 데이터가 들어올 때 마다 ui를 새로고침
 function App() {
-  // 1.변수 counter와 함수 setValue를 선언하고, counter의 초기값을 0으로 설정한 것
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  // onClick 함수는 setValue를 호출하며, 이전 값(prev)을 받아와서 1을 더한 값을 새로운 값으로 설정
-  // 버튼을 클릭할 때마다 counter 값이 1씩 증가하게 됩니다. 
-  // 이 코드는 React 컴포넌트 내부에서 상태를 관리하고 업데이트하는 방법 중 하나입니다.
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
+  // toDo라는 상태 변수를 생성하고, 이를 변경하기 위한 setTodo 함수를 생성
+  const [toDo, setTodo] = useState("");
 
-  // api를 딱 한번만 실행하고 싶을때, useEffacy
-  useEffect(() => {
-    console.log("only");
-  }, []);
+  // seState를 사용하여 toDos라는 상태 변수를 생성하고, 이를 변경하기 위한 setTodos 함수를 생성
+  // toDos 상태 변수는 현재 할 일 목록 정보를 배열 형태로 담게 됩니다. 
+  // setTodos 함수를 사용하여 이 값을 변경할 수 있습니다. 초기 값으로는 빈 배열이 지정
+  const [toDos, setTodos] = useState([]);
 
-  //keyword가 변화할 때 코드를 실행한다.
-  useEffect(() => {
-    console.log("키보드가 입력할때만 실행");
-  }, [keyword]);
+  // onChange 함수를 생성하여, 이 함수가 호출될 때 인자로 전달된 이벤트 객체의 target.value를 이용하여 setTodo 함수를 호출하는 구문
+  const onChange = (e) => setTodo(e.target.value);
 
-  //counter가 변환될때만
-  useEffect(() => {
-    console.log("카운터가 변화할때만 실행");
-  }, [counter]);
+  //  onSubmit 함수를 생성하여, 이 함수가 호출될 때 입력된 내용을 todo 상태 변수에 추가하고, 이를 배열 형태로 담은 toDos 상태 변수를 setTodos 함수를 이용해 변경하는 구문
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    // setTodos 함수를 사용하여 toDos 상태 변수의 값을 변경하고, 이전 상태 값을 유지하도록 currentArray 매개변수를 사용합니다. 이때, toDo 값을 배열의 가장 앞에 추가해야 하므로 spread 연산자를 사용하여 기존 배열에 새로운 값을 추가
+    setTodos(currentArray => [toDo, ...currentArray])
+    //s etTodo 함수를 호출하여, 입력 필드의 값을 초기화합니다. 이렇게 함으로써 사용자가 새로운 할 일을 추가할 때마다 입력 필드가 비워지게 됩니다.
+    setTodo("");
+  }
 
-  //counter가 변환될때만
-  useEffect(() => {
-    console.log("카운터나 키보드 변화할때만 실행");
-  }, [counter, keyword]);
+  console.log(toDos);
 
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..." />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      <h1>my to dos ({toDos.length}) </h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..." />
+        <button>Add To do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item,index) => <li key={index}>{item}</li>)}
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
